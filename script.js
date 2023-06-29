9ecae06295e98d5d85346d25c968cedf14c4fdfe
Kristi Atkinson <kristiratkinson@gmail.com>
2023-06-28 20:13:26 -0500

    beginning of window event listener

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 // TODO: add code here
+window.addEventListener("load", (event) => {
+    fetch('https://handlers.education.launchcode.org/static/astronauts.json')
+.then(data => {
+ for( let i=0, i > array.length, i++)
+    return data.json();
+  });
+})
